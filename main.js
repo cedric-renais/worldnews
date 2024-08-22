c729cd987a98cd7051363abe35b3f159fd3972ed
Cédric RENAIS <cedric.renais@gmail.com>
2024-08-22 14:14:26 +0200

    ♿️ Amélioration de l'accessibilté ⚡️ Amélioration performance

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './sass/style.scss';
 
 // On selectione les éléments du DOM
 const body = document.querySelector('body');
+const main = document.querySelector('main');
 const navOpen = document.querySelector('.nav__open');
 const navClose = document.querySelector('.nav__close');
 const mediaQuery = window.matchMedia('(width < 69.375em)');
@@ -14,6 +15,12 @@ function openMobileMenu() {
   navOpen.setAttribute('aria-expanded', 'true');
   // On ajoute la classe noscroll pour empêcher le scroll de la page
   body.classList.add('noscroll');
+  // On retire l'attribut inert pour permettre l'accès au menu avec le clavier
+  navContent.removeAttribute('inert', '');
+  // On ajoute l'attribut inert pour empêcher l'accès au reste de la page avec le clavier quand le menu est ouvert
+  main.setAttribute('inert', '');
+  // Placement du focus sur le bouton de close à l'ouverture du menu
+  navClose.focus();
 }
 
 // Fonction pour fermer le menu mobile
@@ -22,12 +29,22 @@ function closeMobileMenu() {
   navOpen.setAttribute('aria-expanded', 'false');
   // On retire la classe noscroll pour permettre le scroll de la page
   body.classList.remove('noscroll');
+  // On ajoute l'attribut inert pour empêcher l'accès au menu avec le clavier
+  navContent.setAttribute('inert', '');
+  // On retire l'attribut inert pour permettre l'accès au reste de la page avec le clavier quand le menu est fermé
+  main.removeAttribute('inert', '');
+  // Placement du focus sur le bouton open à la fermeture du menu
+  navOpen.focus();
 }
 
 // Fonction pour gérer l'affichage du menu en fonction de la taille de l'écran
 function setupNav(e) {
   // Si l'écran est de taille mobile
   if (e.matches) {
+    // On ajoute l'attribut inert pour empêcher l'accès au menu avec le clavier
+    navContent.setAttribute('inert', '');
+
+    // On ajoute un setTimeout pour éviter un bug d'affichage
     setTimeout(() => {
       navContent.style.display = 'block';
       navOverlay.style.display = 'block';
@@ -35,13 +52,19 @@ function setupNav(e) {
   }
   // Si l'écran est de taille desktop
   else {
+    // On retire l'attribut inert pour permettre l'accès au menu avec le clavier
+    navContent.removeAttribute('inert', '');
+    main.removeAttribute('inert', '');
+    // On retire les styles display
     navContent.style.display = '';
   }
 }
 
+// On ajoute les écouteurs d'événements
 navOpen.addEventListener('click', openMobileMenu);
 navClose.addEventListener('click', closeMobileMenu);
 
+// On vérifie la taille de l'écran au chargement de la page
 mediaQuery.addEventListener('change', (e) => {
   setupNav(e);
 });
