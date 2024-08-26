f84a2129caa846de540fef405771862d61a8fe49
Cédric RENAIS <cedric.renais@gmail.com>
2024-08-26 12:03:44 +0200

    🔨  Rework du menu de navigation

--- main.js
+++ main.js
@@ -1,76 +1,83 @@
 import './sass/style.scss';
 
-// On selectione les éléments du DOM
-const body = document.querySelector('body');
+///////////////////////////////////////
+// On selectione les éléments du DOM //
+///////////////////////////////////////
+const buttonOpen = document.querySelector('.header__open');
+const buttonClose = document.querySelector('.header__close');
+const mediaQuery = window.matchMedia('(width < 40em)');
+const menu = document.querySelector('.header__menu');
 const main = document.querySelector('main');
-const navOpen = document.querySelector('.nav__open');
-const navClose = document.querySelector('.nav__close');
-const mediaQuery = window.matchMedia('(width < 69.375em)');
-const navContent = document.querySelector('.nav__content');
-const navOverlay = document.querySelector('.nav__overlay');
+const body = document.querySelector('body');
 
-// Fonction pour ouvrir le menu mobile
-function openMobileMenu() {
-  // On ajoute l'attribut aria-expanded pour indiquer que le menu est ouvert
-  navOpen.setAttribute('aria-expanded', 'true');
-  // On ajoute la classe noscroll pour empêcher le scroll de la page
-  body.classList.add('noscroll');
-  // On retire l'attribut inert pour permettre l'accès au menu avec le clavier
-  navContent.removeAttribute('inert', '');
-  // On ajoute l'attribut inert pour empêcher l'accès au reste de la page avec le clavier quand le menu est ouvert
+////////////////////////////////////////////////////////////////
+// Fonction pour ouvrir le menu                               //
+// On change l'attribut aria-expanded pour le bouton sur true //
+// On enlève l'attribut inert sur le menu                     //
+// On enlève l'attribut style pour la transition              //
+// On ajoute l'attribut inert sur le main                     //
+// On ajoute la classe noscroll sur le body                   //
+// On focus sur le bouton de fermeture                        //
+////////////////////////////////////////////////////////////////
+function openMenu() {
+  buttonOpen.setAttribute('aria-expanded', 'true');
+  menu.removeAttribute('inert');
+  menu.removeAttribute('style');
   main.setAttribute('inert', '');
-  // Placement du focus sur le bouton de close à l'ouverture du menu
-  navClose.focus();
+  body.classList.add('noscroll');
+  buttonClose.focus();
 }
 
-// Fonction pour fermer le menu mobile
-function closeMobileMenu() {
-  // On retire l'attribut aria-expanded pour indiquer que le menu est fermé
-  navOpen.setAttribute('aria-expanded', 'false');
-  // On retire la classe noscroll pour permettre le scroll de la page
+/////////////////////////////////////////////////////////////////
+// Fonction pour fermer le menu                                //
+// On change l'attribut aria-expanded pour le bouton sur false //
+// On ajoute l'attribut inert sur le menu                      //
+// On ajoute un setTimeout pour enlever la transition          //
+// On enlève l'attribut inert sur le main                      //
+// On enlève la classe noscroll sur le body                    //
+// On focus sur le bouton d'ouverture                          //
+/////////////////////////////////////////////////////////////////
+function closeMenu() {
+  buttonOpen.setAttribute('aria-expanded', 'false');
+  menu.setAttribute('inert', '');
+  main.removeAttribute('inert');
   body.classList.remove('noscroll');
-  // On ajoute l'attribut inert pour empêcher l'accès au menu avec le clavier
-  navContent.setAttribute('inert', '');
-  // On retire l'attribut inert pour permettre l'accès au reste de la page avec le clavier quand le menu est fermé
-  main.removeAttribute('inert', '');
-  // Placement du focus sur le bouton open à la fermeture du menu
-  navOpen.focus();
+  buttonOpen.focus();
+
+  setTimeout(() => {
+    menu.style.transition = 'none';
+  }, 200);
 }
 
-// Fonction pour gérer l'affichage du menu en fonction de la taille de l'écran
-function setupNav(e) {
-  // Si l'écran est de taille mobile
+///////////////////////////////////////////////////////////
+// On check si le media query pour les mobiles est actif //
+// Si oui, on ajoute l'attribut inert sur le menu        //
+// et on enlève la transition pour éviter les bugs       //
+// Si non, on appelle la fonction closeMenu()            //
+// et on enlève l'attribut inert sur le menu             //
+///////////////////////////////////////////////////////////
+function setupMenu(e) {
   if (e.matches) {
-    // On ajoute l'attribut inert pour empêcher l'accès au menu avec le clavier
-    navContent.setAttribute('inert', '');
-
-    // On ajoute un setTimeout pour éviter un bug d'affichage
-    setTimeout(() => {
-      navContent.style.display = 'block';
-      navOverlay.style.display = 'block';
-    }, 200);
-  }
-  // Si l'écran est de taille desktop
-  else {
-    // On retire l'attribut inert pour permettre l'accès au menu avec le clavier
-    navContent.removeAttribute('inert', '');
-    main.removeAttribute('inert', '');
-    // On retire les styles display
-    navContent.style.display = '';
+    menu.setAttribute('inert', '');
+    menu.style.transition = 'none';
+  } else {
+    closeMenu();
+    menu.removeAttribute('inert');
   }
 }
 
-// On ajoute les écouteurs d'événements
-navOpen.addEventListener('click', openMobileMenu);
-navClose.addEventListener('click', closeMobileMenu);
-
-// On vérifie la taille de l'écran au chargement de la page
-mediaQuery.addEventListener('change', (e) => {
-  setupNav(e);
+/////////////////////////////////////////////////////////////////
+// On appelle la fonction setupMenu() pour initialiser le menu //
+// On ajoute un event listener sur le media query              //
+// Si le media query change, on appelle la fonction setupMenu  //
+/////////////////////////////////////////////////////////////////
+setupMenu(mediaQuery);
+mediaQuery.addEventListener('change', function (e) {
+  setupMenu(e);
 });
 
-// Pour les utilisateurs qui naviguent avec le clavier
-document.addEventListener('keyup', (e) => {
-  if (e.key === 'tab') {
-  }
-});
+///////////////////////////////////////////////////////////////////////////////////////
+// On ajoute les event listeners sur les boutons d'ouverture et de fermeture du menu //
+///////////////////////////////////////////////////////////////////////////////////////
+buttonOpen.addEventListener('click', openMenu);
+buttonClose.addEventListener('click', closeMenu);
